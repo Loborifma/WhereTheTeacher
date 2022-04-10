520b937011969af9ae5fcce3a254413c245e739a
Loborifma <max.f29@mail.ru>
2022-04-10 22:40:06 +0400

    add autofill prefix of sites

--- app/src/main/java/com/example/wheretheteacher/RegisterTeacher.java
+++ app/src/main/java/com/example/wheretheteacher/RegisterTeacher.java
@@ -9,11 +9,19 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Pattern;
+
 public class RegisterTeacher extends AppCompatActivity implements View.OnClickListener {
 
     EditText etName;
     EditText etLink;
     Button btnAdd;
+    StringBuffer stringBuffer = new StringBuffer();
+    String name;
+    String link;
+
+    static final int FIRST_EIGHT_CHAR_HTTPS = 8;
+    static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +37,8 @@ public class RegisterTeacher extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        String name = etName.getText().toString();
-        String link = etLink.getText().toString();
+        name = etName.getText().toString();
+        link = checkOnHttps(etLink.getText().toString());
 
         if(name.isEmpty() || link.isEmpty()){
             Toast.makeText(this, "Одно из полей пустое", Toast.LENGTH_SHORT).show();
@@ -42,6 +50,29 @@ public class RegisterTeacher extends AppCompatActivity implements View.OnClickLi
         intent.putExtra("link", link);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    public String checkOnHttps(String str){
+        String tmp;
+
+        splitWordOnCharHttps(str);
+
+        tmp = stringBuffer.toString();
+
+        if(tmp.equals("https://")){
+            return str;
+        }else if(Pattern.matches("http://.", tmp)){
+            return str;
+        }
+
+        tmp = "https://" + str;
+        return tmp;
+    }
+
+    public void splitWordOnCharHttps(String str){
+        for(int i = 0; i < FIRST_EIGHT_CHAR_HTTPS; i++){
+            stringBuffer.append(str.charAt(i));
+        }
     }
 
 }
